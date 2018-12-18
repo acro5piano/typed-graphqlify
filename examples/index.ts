db5cca1e9d645b26c44736a20aa6d7862cd07ee9
gosho-kazuya <ketsume0211@gmail.com>
2018-12-19 05:32:42 +0900

    add example

---
+++ examples/index.ts
@@ -0,0 +1,53 @@
+import { graphqlify, types } from 'typed-graphqlify'
+
+async function executeGraphql(gqlString: string): Promise<any> {
+  console.log(gqlString)
+}
+
+const getUserQuery = {
+  getUser: {
+    user: {
+      __params: { id: 1 },
+      id: types.number,
+      name: types.string,
+      bankAccount: {
+        id: types.number,
+        branch: types.string,
+      },
+    },
+  },
+}
+
+const gqlString = graphqlify('query', getUserQuery)
+
+console.log(gqlString)
+// =>
+//   query getUser {
+//     user {
+//       id
+//       name
+//       isActive
+//       bankAccount {
+//         id
+//         name
+//       }
+//     }
+//   }
+
+async function run() {
+  // We would like to type this!
+  const result: typeof getUserQuery = await executeGraphql(graphqlify('query', getUserQuery))
+
+  // As we cast `result` to `typeof getUser`,
+  // Now, `result` type looks like this:
+  // interface result {
+  //   getUser: {
+  //     id: number
+  //     name: string
+  //     bankAccount: {
+  //       id: number
+  //       branch: string
+  //     }
+  //   }
+  // }
+}
