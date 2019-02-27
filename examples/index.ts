4da6888165a50464c2aeea598da56b973fb7b321
gosho-kazuya <ketsume0211@gmail.com>
2019-02-27 18:23:25 +0900

    fix export function

--- examples/index.ts
+++ examples/index.ts
@@ -1,4 +1,6 @@
-import { graphqlify, types, GraphQLData } from 'typed-graphqlify'
+/* tslint:disable */
+
+import { params, query, types } from 'typed-graphqlify'
 
 async function executeGraphql(gqlString: string): Promise<any> {
   return 0
@@ -6,19 +8,21 @@ async function executeGraphql(gqlString: string): Promise<any> {
 
 const getUserQuery = {
   getUser: {
-    user: {
-      __params: { id: 1 },
-      id: types.number,
-      name: types.string,
-      bankAccount: {
+    user: params(
+      { id: 1 },
+      {
         id: types.number,
-        branch: types.string,
+        name: types.string,
+        bankAccount: {
+          id: types.number,
+          branch: types.string,
+        },
       },
-    },
+    ),
   },
 }
 
-const gqlString = graphqlify.query(getUserQuery)
+const gqlString = query(getUserQuery)
 
 console.log(gqlString)
 // =>
@@ -35,7 +39,7 @@ console.log(gqlString)
 
 async function run() {
   // We would like to type this!
-  const result: typeof getUserQuery = await executeGraphql(graphqlify.query(getUserQuery))
+  const result: typeof getUserQuery = await executeGraphql(query(getUserQuery))
 
   // As we cast `result` to `typeof getUser`,
   // Now, `result` type looks like this:
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { query, mutation, subscription, alias, rawString } from './graphqlify'
+export { fragment, params, query, mutation, subscription, alias, rawString } from './graphqlify'
 export { types, optional, on } from './types'
