f74896951f662a4ff818f9f9e466fda4c61c5505
gosho-kazuya <ketsume0211@gmail.com>
2020-04-23 10:41:16 +0900

    update lib versions, split tests

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -805,20 +805,4 @@ describe('graphqlify', () => {
       }
     `)
   })
-
-  it('properly errors on invalid input', () => {
-    expect(() => (query as any)('EmptyQuery')).toThrow()
-    expect(() => query({})).toThrow()
-    expect(() => query('Empty', {})).toThrow()
-    expect(() => query('Empty', { hero: {} })).toThrow()
-    expect(() => query('Empty', { hero: [] })).toThrow()
-    expect(() => query('DirectTypes', { hero: { str: 'string' } })).toThrow()
-    expect(() => query('DirectTypes', { hero: { int: 4 } })).toThrow()
-    expect(() => query('DirectTypes', { hero: { float: 3.14 } })).toThrow()
-    expect(() => query('DirectTypes', { hero: { bool: true } })).toThrow()
-    expect(() => query('BadValue', { hero: { prop: null } })).toThrow()
-    expect(() => query('BadValue', { hero: { prop() {} } })).toThrow()
-    expect(() => params('str' as any, {})).toThrow()
-    expect(() => params({}, 'str' as any)).toThrow()
-  })
 })
